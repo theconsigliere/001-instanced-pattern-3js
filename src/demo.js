d932db868fad64371ec33d0afd8604270542adef
Dirty Martini <dirtymartini@Maxwells-MacBook-Pro-2.local>
2023-12-15 08:25:52 +0000

    dat gui sort of works

--- src/demo.js
+++ src/demo.js
@@ -25,57 +25,66 @@ let uTime = { value: 0 }
 // Init Cylinder geometry
 let radius = 2 / 3
 
-let info = {
+const info = {
   grid: 20,
   // SEPERATE THE MESHES A BIT
   // make cellsize bigger than 1
   cellSize: 1.66,
 }
 
+let gridInfo = {
+  totalGridSize: info.grid * info.cellSize,
+  instanceCount: info.grid * info.grid,
+}
+
 // Doesn't work yet
-// gui.add(info, "grid", 5, 100).onChange(function (value) {
-//   info.grid = value
-//   totalGridSize = info.grid * info.cellSize
-//   instanceCount = info.grid * info.grid
-//   instancedGeometry.instanceCount = instanceCount
-// })
+gui.add(info, "grid", 5, 100).onChange(function (value) {
+  updateGeometry()
+})
 
-// gui.add(info, "cellSize", 1.66, 10).onChange(function (value) {
-//   info.cellSize = value
-//   totalGridSize = info.grid * info.cellSize
-// })
-
-let totalGridSize = info.grid * info.cellSize
+gui.add(info, "cellSize", 1.66, 10).onChange(function (value) {
+  updateGeometry()
+})
 
 let geometry = new THREE.CylinderGeometry(radius, radius, 1, 8, 2)
 let instancedGeometry = new THREE.InstancedBufferGeometry().copy(geometry)
-let instanceCount = info.grid * info.grid
-instancedGeometry.instanceCount = instanceCount
 
-// create the instance grid position
-// this is going to hold the position of each instance r us to use inside the shader
+function updateGeometry() {
+  // update values
+  gridInfo.totalGridSize = info.grid * info.cellSize
+  gridInfo.instanceCount = info.grid * info.grid
 
-let pos = new Float32Array(instanceCount * 2) // soo 40 for entire grid + has to hold x and y for each instance (hence instanceCount * 2)
+  instancedGeometry.instanceCount = gridInfo.instanceCount
 
-let i = 0
-for (let y = 0; y < info.grid; y++)
-  for (let x = 0; x < info.grid; x++) {
-    // LETS CENTER THE GRID
-    // to create a grid gap between cylinders we need to multiply the x or y by the cell size (hence * cellSize)
-    // Position the points in the grid from -10 to 10 (hence - totalgridSize / 2)
-    // not quiet center unless we offset by half the grid cell size (hence + cellSize / 2)
+  // create the instance grid position
+  // this is going to hold the position of each instance r us to use inside the shader
 
-    pos[i] = x * info.cellSize - totalGridSize / 2 + info.cellSize / 2
-    pos[i + 1] = y * info.cellSize - totalGridSize / 2 + info.cellSize / 2
-    // jump to next grid position
-    i += 2
-  }
+  let pos = new Float32Array(gridInfo.instanceCount * 2) // soo 40 for entire grid + has to hold x and y for each instance (hence instanceCount * 2)
 
-// attach each position attribute to the instanced geometry
-instancedGeometry.setAttribute(
-  "aPos",
-  new THREE.InstancedBufferAttribute(pos, 2, false)
-)
+  let i = 0
+  for (let y = 0; y < info.grid; y++)
+    for (let x = 0; x < info.grid; x++) {
+      // LETS CENTER THE GRID
+      // to create a grid gap between cylinders we need to multiply the x or y by the cell size (hence * cellSize)
+      // Position the points in the grid from -10 to 10 (hence - totalgridSize / 2)
+      // not quiet center unless we offset by half the grid cell size (hence + cellSize / 2)
+
+      pos[i] =
+        x * info.cellSize - gridInfo.totalGridSize / 2 + info.cellSize / 2
+      pos[i + 1] =
+        y * info.cellSize - gridInfo.totalGridSize / 2 + info.cellSize / 2
+      // jump to next grid position
+      i += 2
+    }
+
+  // attach each position attribute to the instanced geometry
+  instancedGeometry.setAttribute(
+    "aPos",
+    new THREE.InstancedBufferAttribute(pos, 2, false)
+  )
+}
+
+updateGeometry()
 
 // init shaders
 
@@ -185,32 +194,32 @@ mesh.scale.y = 10
 // flip mesh to look at us
 mesh.rotation.z = -Math.PI / 2
 // push our mesh back by half the gridSize
-mesh.position.x = -totalGridSize / 2 - 5
+mesh.position.x = -gridInfo.totalGridSize / 2 - 5
 rendering.scene.add(mesh)
 
 // bottom mesh: (on its side) and move to bottom
 let meshBotton = new THREE.Mesh(instancedGeometry, material)
 meshBotton.scale.y = 10
-meshBotton.position.y = -totalGridSize / 2 - 5
+meshBotton.position.y = -gridInfo.totalGridSize / 2 - 5
 rendering.scene.add(meshBotton)
 
 // mesh top: rotate it to look down and move it to top
 let meshTop = new THREE.Mesh(instancedGeometry, material)
 meshTop.scale.y = 10
 meshTop.rotation.z = Math.PI
-meshTop.position.y = totalGridSize / 2 + 5
+meshTop.position.y = gridInfo.totalGridSize / 2 + 5
 rendering.scene.add(meshTop)
 
 let meshRight = new THREE.Mesh(instancedGeometry, material)
 meshRight.scale.y = 10
 meshRight.rotation.x = Math.PI / 2
-meshRight.position.z = -totalGridSize / 2 - 5
+meshRight.position.z = -gridInfo.totalGridSize / 2 - 5
 rendering.scene.add(meshRight)
 
 let meshLeft = new THREE.Mesh(instancedGeometry, material)
 meshLeft.scale.y = 10
 meshLeft.rotation.x = -Math.PI / 2
-meshLeft.position.z = totalGridSize / 2 + 5
+meshLeft.position.z = gridInfo.totalGridSize / 2 + 5
 rendering.scene.add(meshLeft)
 
 // Events
